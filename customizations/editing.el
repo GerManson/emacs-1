6647050dd324ea121e7311d71d5a74ecb342b492
Francisco Soto <ebobby@gmail.com>
2011-05-09 12:36:27 -0700

    Added a prompt when exiting emacs to make sure that's what I wanted to do.

--- customizations/editing.el
+++ customizations/editing.el
@@ -34,3 +34,13 @@
 ; wrap lines in a tasteful way
 (global-visual-line-mode 1)
 
+; ask before exiting
+(defun confirm-exit-emacs ()
+        "ask for confirmation before exiting emacs"
+        (interactive)
+        (if (yes-or-no-p "Are you sure you want to exit? ")
+                (save-buffers-kill-emacs)))
+
+(global-unset-key "\C-x\C-c")
+(global-set-key "\C-x\C-c" 'confirm-exit-emacs)
+
