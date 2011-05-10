aebbcce3d40a633401e94695895cc8645a74ba34
Francisco Soto <ebobby@gmail.com>
2011-05-10 00:13:34 -0700

    Truncate lines configuration

--- customizations/buffers.el
+++ customizations/buffers.el
@@ -22,3 +22,8 @@
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
   (flet ((process-list ())) ad-do-it))
+
+; line wrapping
+(set-default 'truncate-lines t)
+(setq truncate-partial-width-windows nil)
+
