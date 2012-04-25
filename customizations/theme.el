6fec6ef0956216033c30b82d7477b64066cf3f43
Francisco Soto <ebobby@gmail.com>
2012-04-25 15:21:33 -0700

    Added font for Windows.

--- customizations/theme.el
+++ customizations/theme.el
@@ -9,4 +9,7 @@
   (color-theme-almost-monokai)
 
   (if (string-equal system-type "gnu/linux")
-    (set-default-font "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")))
+    (set-default-font "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
+
+  (if (string-equal system-type "windows-nt")
+      (set-face-attribute 'default nil :font "Consolas-11")))
