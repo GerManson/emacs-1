821069bf6d462da564942a774811312cc1715a43
Francisco Soto <ebobby@gmail.com>
2012-05-04 01:40:58 -0700

    Added a simple function to help with blocks of comments.

--- customizations/misc.el
+++ customizations/misc.el
@@ -1,1 +1,12 @@
 (put 'erase-buffer 'disabled nil)
+
+;; Just a small routine to help with comment blocks.
+(defun fill-and-comment ()
+  "Fills and comments the region."
+  (interactive)
+  (save-excursion
+    (let ((beg (mark))
+          (end (point)))
+      (fill-region beg end)
+      (comment-region beg end))))
+
