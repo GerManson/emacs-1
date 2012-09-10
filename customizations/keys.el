fc2c69f08796a5ca5d8d47d30c7d49ab694ecdeb
Francisco Soto <ebobby@gmail.com>
2012-09-09 17:40:24 -0700

    Changed so CMD is Meta in MacOSX.

--- customizations/keys.el
+++ customizations/keys.el
@@ -2,3 +2,7 @@
 
 ; This function is used too many times, using Meta slows me down
 (global-set-key "\C-x\C-m" 'execute-extended-command)
+
+(when (eq system-type 'darwin)
+  (setq mac-option-modifier 'none)
+  (setq mac-command-modifier 'meta))
