3fc3ec30896ee5cc97de590c3096df5c7f973494
Francisco Soto <ebobby@ebobby.org>
2013-05-21 19:06:34 -0700

    Small refactor.

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; fms's init.el
-(require 'cl)
+(require 'cl-lib)
 
 (require 'package)
 (add-to-list 'package-archives
--- utilities/vendor.el
+++ utilities/vendor.el
@@ -3,12 +3,8 @@
   (let* ((file (symbol-name library))
          (normal (concat "~/.emacs.d/vendor/" file))
          (suffix (concat normal ".el")))
-    (cond
-     ((file-directory-p normal)
-      (add-to-list 'load-path normal)
-      (require library))
-     ((file-directory-p suffix)
-      (add-to-list 'load-path suffix)
-      (require library))
-     ((file-exists-p suffix)
-      (require library)))))
+    (when (cond
+           ((file-directory-p normal) (add-to-list 'load-path normal))
+           ((file-directory-p suffix) (add-to-list 'load-path suffix))
+           ((file-exists-p suffix) t))
+      (require library))))
