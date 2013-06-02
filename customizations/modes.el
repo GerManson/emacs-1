b53272a1dd0f80b68f0353295d12aeefddc83977
Francisco Soto <ebobby@ebobby.org>
2013-06-02 16:04:15 -0700

    Added hotkeys to send stuff from js buffers to a running js instance.

--- customizations/modes.el
+++ customizations/modes.el
@@ -35,6 +35,10 @@
 (autoload 'js2-mode "js" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-hook 'js2-mode-hook '(lambda ()
+                            (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+                            (local-set-key "\C-cb" 'js-send-buffer)
+                            (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)))
 
 (require 'js-comint)
 (cond ((eq system-type 'darwin) (progn
