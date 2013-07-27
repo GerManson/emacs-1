284c20410eb39afdec303a9fffe681800dcfef98
Francisco Soto <ebobby@ebobby.org>
2013-07-26 20:30:38 -0700

    Changed theme.

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
                     magit
                     markdown-mode
                     monokai-theme
+                    twilight-anti-bright-theme
                     paredit
                     php-mode
                     popup
@@ -64,19 +65,3 @@
 
 ; load personal customizations (keybindings, colors, etc.)
 (mapcar 'load-directory '("~/.emacs.d/customizations"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
- '(ansi-term-color-vector [unspecific "#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"])
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "afc97ff0c2646731990407327f272f777df252ea302a0f97998f4cc6233465ad" default)))
- '(fci-rule-color "#073642"))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
