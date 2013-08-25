fbf812dc7b8c1a2d114d7c3e0454f1dd648d24f4
Francisco Soto <ebobby@ebobby.org>
2013-08-25 16:14:04 -0700

    Some more bindings.

--- customizations/bindings.el
+++ customizations/bindings.el
@@ -11,22 +11,24 @@
 ;; Hotkey for truncating lines
 (global-set-key (kbd "<f7>") 'toggle-truncate-lines)
 
+;; Window management
 (global-set-key (kbd "M-1") 'delete-other-windows)
 (global-set-key (kbd "M-2") 'split-window-horizontally)
 (global-set-key (kbd "M-3") 'split-window-vertically)
 (global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-o") 'other-window)
-(global-set-key (kbd "M-O") (lambda ()
-                              (interactive)
-                              (other-window -1)))
+(global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1)))
 
 (global-unset-key (kbd "C-x 1"))
 (global-unset-key (kbd "C-x 2"))
 (global-unset-key (kbd "C-x 3"))
 (global-unset-key (kbd "C-x 0"))
 (global-unset-key (kbd "C-x o"))
+(global-unset-key (kbd "C-x o"))
 
+;; Kill buffers
 (global-set-key (kbd "M-k") 'kill-this-buffer)
+(global-unset-key (kbd "C-x k"))
 
 ;; SMEX
 (global-set-key "\C-x\C-m" 'smex)
