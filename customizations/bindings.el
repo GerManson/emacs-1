13893370cafc9137e81e11b93bf655e5ac507207
Francisco Soto <ebobby@ebobby.org>
2013-08-27 00:18:29 -0700

    Switched bindings.

--- customizations/bindings.el
+++ customizations/bindings.el
@@ -13,8 +13,8 @@
 
 ;; Window management
 (global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "M-2") 'split-window-horizontally)
-(global-set-key (kbd "M-3") 'split-window-vertically)
+(global-set-key (kbd "M-2") 'split-window-vertically)
+(global-set-key (kbd "M-3") 'split-window-horizontally)
 (global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1)))
