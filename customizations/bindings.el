e8e3e0eea7c0f9a3c7f946405b424c3b98ac069a
Francisco Soto <ebobby@ebobby.org>
2013-09-07 12:54:11 -0700

    Changed the way to set my own bindings.
    Now major modes won't override my keybindings because they are set in a
    custom minor mode.

--- customizations/bindings.el
+++ customizations/bindings.el
@@ -4,35 +4,53 @@
 ;; Francisco Soto <ebobby@ebobby.org>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; New way to get out, just to make sure we didnt screw up.
-(global-unset-key "\C-x\C-c")
-(global-set-key "\C-x\C-c" 'confirm-exit-emacs)
+(defvar my-key-bindings-minor-mode-map (make-keymap) "My keymap.")
 
-;; Hotkey for truncating lines
-(global-set-key (kbd "<f7>") 'toggle-truncate-lines)
-
-;; Window management
-(global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "M-2") 'split-window-vertically)
-(global-set-key (kbd "M-3") 'split-window-horizontally)
-(global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-o") 'other-window)
-(global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1)))
-
+;; Unset a bunch of keys
+(global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x 1"))
 (global-unset-key (kbd "C-x 2"))
 (global-unset-key (kbd "C-x 3"))
 (global-unset-key (kbd "C-x 0"))
 (global-unset-key (kbd "C-x o"))
 (global-unset-key (kbd "C-x o"))
-
-;; Kill buffers
-(global-set-key (kbd "M-k") 'kill-this-buffer)
 (global-unset-key (kbd "C-x k"))
 
+;; New way to get out, just to make sure we didnt screw up.
+(define-key my-key-bindings-minor-mode-map (kbd "C-x C-c") 'confirm-exit-emacs)
+
+;; Hotkey for truncating lines
+(define-key my-key-bindings-minor-mode-map (kbd "<f7>") 'toggle-truncate-lines)
+
+;; Window management
+(define-key my-key-bindings-minor-mode-map (kbd "M-1") 'delete-other-windows)
+(define-key my-key-bindings-minor-mode-map (kbd "M-2") 'split-window-vertically)
+(define-key my-key-bindings-minor-mode-map (kbd "M-3") 'split-window-horizontally)
+(define-key my-key-bindings-minor-mode-map (kbd "M-0") 'delete-window)
+(define-key my-key-bindings-minor-mode-map (kbd "M-o") 'other-window)
+(define-key my-key-bindings-minor-mode-map (kbd "M-O") (lambda () (interactive) (other-window -1)))
+
+;; Kill buffers
+(define-key my-key-bindings-minor-mode-map (kbd "M-k") 'kill-this-buffer)
+
 ;; SMEX
-(global-set-key "\C-x\C-m" 'smex)
+(define-key my-key-bindings-minor-mode-map  (kbd "C-x C-m") 'smex)
+
 ;; M-x is now smex localized for major mode
-(global-set-key (kbd "M-x") 'smex-major-mode-commands)
+(define-key my-key-bindings-minor-mode-map (kbd "M-x") 'smex-major-mode-commands)
+
 ;; Old M-X just in case.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(define-key my-key-bindings-minor-mode-map (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(define-minor-mode my-key-bindings-minor-mode
+  "A minor mode that sets my own key bindings globally." t " my-keys" 'my-key-bindings-minor-mode
+  (toggle-truncate-lines))
+
+(my-key-bindings-minor-mode 1)
+
+(defun my-minibuffer-setup-hook ()
+  "This turns the my-keys mode off."
+  (my-keys-minor-mode 0))
+
+;; In case the keybindings are not good in the minibuffer.
+;;(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
