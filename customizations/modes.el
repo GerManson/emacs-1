769c3e05ec77ff54f7804931fa9a206b4b848384
Francisco Soto <ebobby@ebobby.org>
2013-09-07 12:59:05 -0700

    No longer needed. The my-keys-binding minor mode takes care of it.

--- customizations/modes.el
+++ customizations/modes.el
@@ -61,9 +61,6 @@
      (set-face-background 'highlight nil) ;; highlight is overriding other background colors for diff chunks
      (set-face-foreground 'highlight nil) ;; highlight is overriding other foreground colors for diff chunks
      (set-face-underline  'highlight nil)
-     (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
-     (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
-     (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
      (define-key magit-mode-map (kbd "<tab>") 'magit-toggle-section)    ; was smart-tab
      (define-key magit-status-mode-map (kbd "M-K") 'magit-quit-session)
      (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace) ))
