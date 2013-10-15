922a9a48f67f39d51ca4bcec33869c163c8d946c
Francisco Soto <ebobby@ebobby.org>
2013-10-15 11:09:36 -0700

    Smarter tabs.

--- customizations/bindings.el
+++ customizations/bindings.el
@@ -6,6 +6,46 @@
 
 (defvar my-key-bindings-minor-mode-map (make-keymap) "My keymap.")
 
+(defvar smart-tab-using-hippie-expand t
+  "Turn this on if you want to use hippie-expand completion.")
+
+(defun smart-tab (prefix)
+  "Needs `transient-mark-mode' to be on. This smart tab is
+  minibuffer compliant: it acts as usual in the minibuffer.
+
+  In all other buffers: if PREFIX is \\[universal-argument], calls
+  `smart-indent'. Else if point is at the end of a symbol,
+  expands it. Else calls `smart-indent'."
+  (interactive "P")
+  (cl-labels ((smart-tab-must-expand (&optional prefix)
+                                     (unless (or (consp prefix)
+                                                 mark-active)
+                                       (looking-at "\\_>"))))
+    (cond ((minibufferp)
+           (minibuffer-complete))
+          ((smart-tab-must-expand prefix)
+           (if smart-tab-using-hippie-expand
+               (hippie-expand prefix)
+             (dabbrev-expand prefix)))
+          ((smart-tab-fallback)))))
+
+(defun smart-indent ()
+  "Indents region if mark is active, or current line otherwise."
+  (interactive)
+  (if mark-active
+      (indent-region (region-beginning) (region-end))
+    (indent-for-tab-command)))
+
+(defun smart-tab-fallback ()
+  (let* ((my-key-bindings-minor-mode nil)
+         (keys (this-single-command-keys))
+         (original-command (key-binding keys t)))
+    (if original-command
+        (progn
+          (setq this-original-command original-command)
+          (call-interactively original-command))
+      (smart-indent))))
+
 ;; Unset a bunch of keys
 (global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x 1"))
@@ -42,6 +82,10 @@
 ;; Old M-X just in case.
 (define-key my-key-bindings-minor-mode-map (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;; Smart tab
+(define-key my-key-bindings-minor-mode-map (kbd "TAB") 'smart-tab)
+(define-key my-key-bindings-minor-mode-map [(tab)] 'smart-tab)
+
 (define-minor-mode my-key-bindings-minor-mode
   "A minor mode that sets my own key bindings globally." t " my-keys" 'my-key-bindings-minor-mode
   (toggle-truncate-lines))
@@ -54,3 +98,13 @@
 
 ;; In case the keybindings are not good in the minibuffer.
 ;;(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+
+;; Make sure this mode has preference over all modes.
+(defadvice load (after give-my-keybindings-priority)
+  "Try to ensure that my keybindings always have priority."
+  (if (not (eq (car (car minor-mode-map-alist)) 'my-key-bindings-minor-mode))
+      (let ((mykeys (assq 'my-key-bindings-minor-mode minor-mode-map-alist)))
+        (assq-delete-all 'my-key-bindings-minor-mode minor-mode-map-alist)
+        (add-to-list 'minor-mode-map-alist mykeys))))
+
+(ad-activate 'load)
